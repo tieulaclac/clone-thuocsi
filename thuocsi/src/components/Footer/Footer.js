25fc5eb9d062c535e030bc324cd48d624ff89114
Hoan <nvhoan911@gmail.com>
2021-08-04 17:12:37 +0700

    khung base thuocsi

--- thuocsi/src/components/Footer/Footer.js
+++ thuocsi/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./footer.css"
 export default function Footer() {
     return (
         <div className="footer"> 
-            <p>Footer</p>
+            <h2>Footer</h2>
         </div>
     )
 }
--- thuocsi/src/pages/Home/Home.js
+++ thuocsi/src/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import "./home.css";
 import Layout from "../../layout/Layout";
 import Slider from "./components/slider/Slider";
-import Sanpham from "./components/sanpham/sanpham";
-import Doitac from "./components/doitac/doitac";
+import Sanpham from "./components/sanpham/Sanpham";
+import Doitac from "./components/doitac/Doitac";
 import Danhgia from "./components/danhgia/Danhgia";
 import Truyenthong from "./components/truyenthong/Truyenthong";
 
